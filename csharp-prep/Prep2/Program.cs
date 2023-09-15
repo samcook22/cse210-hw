aeddf35eb95bbd5e7f9c35e4672e298d7c24510d
samcook22 <144865298+samcook22@users.noreply.github.com>
2023-09-14 21:30:33 -0600

    Update Program.cs
    Couldn't find out how to push.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,54 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string letter;
+        Console.WriteLine("");
+        Console.Write("What is your grade percentage? ");
+        string input = Console.ReadLine();
+        int number = int.Parse(input);
+        if (number >= 90 && number <= 100)
+        {
+            letter = "A";
+        }
+        else if (number >= 80 && number <= 90)
+        {
+            letter = "B";
+        }
+        else if (number >= 70 && number <= 80)
+        {
+            letter = "C";
+        }
+        else if (number >= 60 && number <= 70)
+        {
+            letter = "D";
+        }
+        else if (number >= 0 && number <= 60)
+        {
+            letter = "F";
+        }
+        else
+        {
+            Console.WriteLine("Invalid input!");
+            Console.WriteLine("");
+            return;
+        }
+        Console.Write($"You got a(n) {letter}. ");
+        Console.WriteLine("");
+        if (letter == "A" || letter == "B" || letter == "C")
+        {
+            Console.Write("Great job! You acheived a passing grade!");
+            Console.WriteLine("");
+            Console.WriteLine("");
+        }
+        else
+        {
+            Console.Write("Nice try! Keep studying hard and maybe you'll get a passing grade next time.");
+            Console.WriteLine("");
+            Console.WriteLine("");
+        }
     }
 }
