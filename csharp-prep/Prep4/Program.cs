21e3137f87331bcc9a8f7de493d1524e0d6ab67f
Sam Cook <coo22013@byuiu.edu>
2023-09-20 17:55:27 -0600

    Prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,38 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        List<int> numbers = new List<int>();
+        while (true)
+        {
+            Console.WriteLine("");
+            Console.Write("Enter number: ");
+            string input = Console.ReadLine();
+            int number = int.Parse(input);
+            numbers.Add(number);
+            if (number == 0)
+            {
+                break;
+            }
+        
+        }
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine("");
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine("");
+        float average = (float)sum / (numbers.Count - 1);
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine("");
+        int max = numbers.Max();
+        Console.WriteLine($"The largest number is: {max}");
+        Console.WriteLine("");
     }
 }
