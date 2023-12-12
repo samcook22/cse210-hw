9358936bf4505085109422019d9a72f035c7e6ba
samcook22 <144865298+samcook22@users.noreply.github.com>
2023-12-12 03:27:03 -0700

    Commit

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,68 +1,66 @@
 using System;
 
-namespace MeditationProgram
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main()
+        int breatheCount = 0;
+        int reflectCount = 0;
+        int listCount = 0;
+
+        Console.WriteLine("Welcome to the Mindfulness Program!");
+        Console.WriteLine();
+        bool running = true;
+        while (running)
         {
-            Console.WriteLine("Welcome to the meditation program!");
-            Console.WriteLine("How many seconds would you like to meditate for?");
-            int minutes = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please select one of the following activities: ");
+            Console.WriteLine("1. Breathing Activity");
+            Console.WriteLine("2. Reflecting Activity");
+            Console.WriteLine("3. Listing Activity");
+            Console.WriteLine("4. Quit");
+            Console.WriteLine();
+            Console.Write("What would you like to do? ");
+            int choice = int.Parse(Console.ReadLine());
+            Console.WriteLine();
 
-            int milliseconds = minutes * 60 * 1000;
-            DateTime end = DateTime.Now + new TimeSpan(0, 0, 0, 0, milliseconds);
+            switch (choice)
+            {   
 
-            editationProgram meditationProgram = new editationProgram();
+                case 1:
+                    Console.Clear();
+                    BreathingActivity breathing = new BreathingActivity();
+                    breathing.RunBreathingActivity();
+                    breatheCount++;
+                    break;
 
-            while (DateTime.Now < end)
-            {
-                Console.Clear();
-                Console.WriteLine("Choose an activity:");
-                Console.WriteLine("1. Breathing Activity");
-                Console.WriteLine("2. Reflection Activity");
-                Console.WriteLine("3. Listing Activity");
-                Console.WriteLine("4. Quit");
+                case 2:
+                    Console.Clear();
+                    ReflectingActivity reflection = new ReflectingActivity();
+                    reflection.RunReflectingActivity();
+                    reflectCount++;
+                    break;
 
-                int activity = int.Parse(Console.ReadLine());
+                case 3:
+                    Console.Clear();
+                    ListingActivity listing = new ListingActivity();
+                    listing.RunListingActivity();
+                    listCount++;
+                    break;
 
-                switch (activity)
-                {
-                    case 1:
-                        meditationProgram.Breathe();
-                        break;
-                    case 2:
-                        Console.WriteLine("Think of a time when you were successful or demonstrated strength.");
-                        System.Threading.Thread.Sleep(5000);
-                        Console.WriteLine("How are you different because of that experience?");
-                        System.Threading.Thread.Sleep(5000);
-                        Console.WriteLine("What can you do to have more of those experiences?");
-                        System.Threading.Thread.Sleep(5000);
-                        break;
-                    case 3:
-                        Console.WriteLine("List as many things as you can in a certain area of strength or positivity.");
-                        System.Threading.Thread.Sleep(8000);
-                        Console.WriteLine("List as many things as you can on what you are not doing, but what you should be doing.");
-                        System.Threading.Thread.Sleep(8000);
-                        Console.WriteLine("List as many things as you can on what you are doing that you should stop doing.");
-                        System.Threading.Thread.Sleep(8000);
-                        break;
-                    case 4:
-                        Console.WriteLine("Thank you for meditating with us!");
-                        Console.ReadKey();
-                        return;
-                    default:
-                        Console.WriteLine("Invalid option selected. Please choose again.");
-                        break;
-                }
-
-                meditationProgram.Quote();
-                Console.WriteLine("Press any key to continue...");
-                Console.ReadKey();
+                case 4:
+                    running = false;
+                    Console.WriteLine("Great job! You completed the following activities: ");
+                    Console.WriteLine($"Breathing Activity: {breatheCount} times");
+                    Console.WriteLine($"Reflecting Activity: {reflectCount} times");
+                    Console.WriteLine($"Listing Activity: {listCount} times");
+                    Console.WriteLine();
+                    Console.WriteLine("Thank you. Have a nice day!");
+                    break;
+                default:
+                    Console.WriteLine("Invalid input. Please choose one of the following activities.");
+                    break;
             }
-
-            Console.WriteLine("Meditation program ended. Press any key to exit...");
-            Console.ReadKey();
+     
         }
     }
 }
