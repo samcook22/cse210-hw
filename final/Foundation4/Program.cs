27d93908b357d7e262703af1599b845dff25ca63
samcook22 <144865298+samcook22@users.noreply.github.com>
2023-12-12 03:52:31 -0700

    Commit

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,89 +1,26 @@
 using System;
 
-// Abstract base class representing a vehicle
-public abstract class Vehicle
-{
-    private string brand;
-    private string model;
-
-    public Vehicle(string brand, string model)
-    {
-        this.brand = brand;
-        this.model = model;
-    }
-
-    public void DisplayInfo()
-    {
-        Console.WriteLine($"Brand: {brand}, Model: {model}");
-    }
-
-    // Abstract method to be overridden by derived classes
-    public abstract void Start();
-}
-
-// Derived class 1: Car
-public class Car : Vehicle
-{
-    private int numberOfDoors;
-
-    public Car(string brand, string model, int numberOfDoors) : base(brand, model)
-    {
-        this.numberOfDoors = numberOfDoors;
-    }
-
-    // Override the Start method for cars
-    public override void Start()
-    {
-        Console.WriteLine($"Starting the car with {numberOfDoors} doors.");
-    }
-
-    // Unique method for cars
-    public void Drive()
-    {
-        Console.WriteLine("Driving the car.");
-    }
-}
-
-// Derived class 2: Motorcycle
-public class Motorcycle : Vehicle
-{
-    private bool hasSidecar;
-
-    public Motorcycle(string brand, string model, bool hasSidecar) : base(brand, model)
-    {
-        this.hasSidecar = hasSidecar;
-    }
-
-    // Override the Start method for motorcycles
-    public override void Start()
-    {
-        Console.WriteLine($"Starting the motorcycle{(hasSidecar ? " with a sidecar" : "")}.");
-    }
-
-    // Unique method for motorcycles
-    public void Ride()
-    {
-        Console.WriteLine("Riding the motorcycle.");
-    }
-}
-
 class Program
 {
-    static void Main()
+    static void Main(string[] args)
     {
-        // Create instances of the derived classes
-        Car myCar = new Car("Toyota", "Camry", 4);
-        Motorcycle myMotorcycle = new Motorcycle("Harley-Davidson", "Sportster", false);
+        List<Exercise> exerciseList = new List<Exercise>();
+        
+        Running running = new Running("July 15, 2023", 30, 4.8);
+        exerciseList.Add(running);
 
-        // Demonstrate abstraction, encapsulation, inheritance, and polymorphism
-        myCar.DisplayInfo();
-        myCar.Start();
-        myCar.Drive();
+        StationaryBicycles stationaryBicycles = new StationaryBicycles("July 17, 2023", 60, 15);
+        exerciseList.Add(stationaryBicycles);
 
+        Swimming swimming = new Swimming("July 24, 2023", 20, 20);
+        exerciseList.Add(swimming);
+
+        Console.WriteLine("Exercises:");
         Console.WriteLine();
 
-        myMotorcycle.DisplayInfo();
-        myMotorcycle.Start();
-        myMotorcycle.Ride();
+        foreach (Exercise exercise in exerciseList) 
+        {
+            Console.WriteLine(exercise.GetSummary());
+        }
     }
 }
